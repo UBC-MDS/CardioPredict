6c8c9ddc47c0b88250be8011deae50edeb73ea89
doris531 <momotaru2012@gmail.com>
2023-11-23 13:06:34 -0800

    fix directory path

--- src/split_prrepro_test.py
+++ src/split_prrepro_test.py
@@ -1,29 +1,12 @@
 # author: Doris Wang
 # date: 2023-11-23
 
-"""
-Split and pre-process the CardioPredict dataset from "https://clinicaltrials.gov/study/NCT00005121"
-Writes the training (processed) and test (unprocessed) data to separate CSV files.
-
-Usage:
-split_prepro_test.py --input_file=<input_file> --output_train_file=<output_train_file> --output_test_file=<output_test_file>
-
-Options:
---input_file=<input_file>                     Path of the input file that contains the raw data.
---output_train_file=<output_train_file>       Path of the output file which will contain the CSV train data.
---output_test_file=<output_test_file>         Path of the output file which will contain the CSV test data.
-
-Command to run the script:
-python src/split_prepro_test.py --input_file="./data/framinghan.csv" --output_train_file="./data/processed/train_df.csv" --output_test_file="./data/processed/test_df.csv"
-"""
-
 import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.compose import make_column_transformer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from docopt import docopt
 
 def preprocess_and_split_data(input_file, output_train_file, output_test_file, numeric_features, categorical_features, test_size=0.2, random_state=123):
     """
@@ -70,19 +53,17 @@ def preprocess_and_split_data(input_file, output_train_file, output_test_file, n
     train_df_processed.to_csv(output_train_file, index=False)
     test_df.to_csv(output_test_file, index=False)
 
-if __name__ == "__main__":
-    opt = docopt(__doc__)
-    preprocess_and_split_data(opt["--input_file"], opt["--output_train_file"], opt["--output_test_file"],
-                              ['AGE', 'FRW', 'SBP', 'DBP', 'CHOL', 'CIG'],  # Example numeric features
-                              ['sex'])  # Example categorical feature
-
-# Tests
-
+# Test Function
 def test_preprocess_and_split_data():
     # Paths for test files
-    test_input_file = "data/framingham.csv"
+    test_input_file = "data/test_framingham.csv"
     test_output_train_file = "data/processed/train_df.csv"
     test_output_test_file = "data/processed/test_df.csv"
+
+    # Check if the processed directory exists, create if not
+    processed_dir = os.path.join(project_dir, "data/processed")
+    if not os.path.exists(processed_dir):
+        os.makedirs(processed_dir)
 
     # Sample numeric and categorical features (adjust as per your dataset)
     numeric_features = ['AGE', 'FRW', 'SBP', 'DBP', 'CHOL', 'CIG']
@@ -111,14 +92,35 @@ def test_preprocess_and_split_data():
     assert len(train_df) == expected_train_size, "Training data size is incorrect."
     assert len(test_df) == expected_test_size, "Test data size is incorrect."
 
-     # Assert preprocessing on training data
+    # Assert preprocessing on training data
     # Check if numeric features are scaled
     scaler = StandardScaler()
-    scaled_numeric = scaler.fit_transform(original_df[numeric_features])
+    scaler.fit(original_df[numeric_features])
+    scaled_numeric = scaler.transform(original_df[numeric_features])
     assert all(train_df[numeric_features].mean().abs() < 0.1), "Numeric features are not properly scaled in training data."
 
     # Check if categorical features are one-hot encoded
     encoder = OneHotEncoder(drop="if_binary", sparse_output=False)
-    encoded_categorical = encoder.fit_transform(original_df[categorical_features])
+    encoder.fit(original_df[categorical_features])
+    encoded_categorical = encoder.transform(original_df[categorical_features])
     encoded_feature_names = encoder.get_feature_names_out(categorical_features)
     assert all(name in train_df.columns for name in encoded_feature_names), "Categorical features are not properly one-hot encoded in training data."
+
+if __name__ == "__main__":
+    # Get the directory of the current script
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    project_dir = os.path.dirname(script_dir)
+
+    # Construct the full file paths
+    input_file = os.path.join(project_dir, "data/framingham.csv")
+    output_train_file = os.path.join(project_dir, "data/processed/train_df.csv")
+    output_test_file = os.path.join(project_dir, "data/processed/test_df.csv")
+
+    # Example usage with the full paths
+    preprocess_and_split_data(input_file, 
+                              output_train_file, 
+                              output_test_file,
+                              ['AGE', 'FRW', 'SBP', 'DBP', 'CHOL', 'CIG'],  # Example numeric features
+                              ['sex'])  # Example categorical feature
+    print('All tests passed! 👍')
+
