c4bc193cf2013a900917696b32f00f539a8a9b27
He Ma <hema2022@student.ubc.ca>
2023-12-06 11:30:56 -0800

    Add example to logistic_regression_evaluation.py and change the output format to dict

--- src/logistic_regression_evaluation.py
+++ src/logistic_regression_evaluation.py
@@ -1,25 +1,16 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.pipeline import make_pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import make_column_transformer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import ConfusionMatrixDisplay, classification_report
-
-
+from sklearn.metrics import confusion_matrix, classification_report
 
 def evaluate_logistic_regression(X_train, y_train, X_test, y_test, numeric_features, categorical_features):
     """
     Evaluate the logistic regression model with hyperparameter tuning using GridSearchCV.
-
-    This function processes the training data and evaluates the logistic regression model on the test data.
-    It creates a preprocessing pipeline for numeric and categorical features and performs hyperparameter 
-    tuning to find the best model. It then outputs the confusion matrix and classification report for 
-    the test data, along with  the fitted GridSearchCV object and the results of the GridSearchCV.
 
     Parameters:
     - X_train (pd.DataFrame): Training data features.
@@ -30,11 +21,8 @@ def evaluate_logistic_regression(X_train, y_train, X_test, y_test, numeric_featu
     - categorical_features (list): List of column names for categorical features.
 
     Returns:
-    - cm (numpy.ndarray): Confusion matrix as a 2D numpy array.
-    - report (str): Text summary of the precision, recall, F1 score, and support for each class.
-    - gs (GridSearchCV): Fitted GridSearchCV object.
-    - gs_res (pd.DataFrame): DataFrame containing the cross-validation results from GridSearchCV,
-      sorted by the rank of the test score.
+    - results (dict): Dictionary containing the confusion matrix, classification report,
+                      fitted GridSearchCV object, and GridSearchCV results.
     """
 
     # Create pipeline for numeric features
@@ -55,16 +43,44 @@ def evaluate_logistic_regression(X_train, y_train, X_test, y_test, numeric_featu
     gs.fit(X_train, y_train)
 
     gs_res = pd.DataFrame(gs.cv_results_)[[
-    'rank_test_score', 'param_logisticregression__C', 
-    'mean_train_score', 'std_train_score', 'mean_test_score', 'std_test_score']].sort_values('rank_test_score')
-
-
- 
+        'rank_test_score', 'param_logisticregression__C', 
+        'mean_train_score', 'std_train_score', 'mean_test_score', 'std_test_score']].sort_values('rank_test_score')
 
     # Generate the confusion matrix
     cm = confusion_matrix(y_test, gs.predict(X_test))
 
     # Generate and print classification report
-    report = classification_report(y_test, gs.predict(X_test), zero_division = 1)
+    report = classification_report(y_test, gs.predict(X_test), zero_division=1)
 
-    return cm, report, gs, gs_res
+    # Returning results in a dictionary
+    results = {
+        'confusion_matrix': cm,
+        'classification_report': report,
+        'grid_search_object': gs,
+        'grid_search_results': gs_res
+    }
+
+    return results
+
+# Example Usage
+'''
+# Assuming data is loaded into X_train, y_train, X_test, y_test
+# Example of numeric and categorical features
+numeric_features = ['age', 'income']
+categorical_features = ['gender', 'occupation']
+
+# Call the function
+results = evaluate_logistic_regression(X_train, y_train, X_test, y_test, numeric_features, categorical_features)
+
+# Accessing different components of the result
+conf_matrix = results['confusion_matrix']
+class_report = results['classification_report']
+grid_search_cv_object = results['grid_search_object']
+grid_search_cv_results = results['grid_search_results']
+
+# Example of how to use these components
+print("Confusion Matrix:\n", conf_matrix)
+print("\nClassification Report:\n", class_report)
+print("\nBest Parameters from Grid Search:\n", grid_search_cv_object.best_params_)
+'''
+
